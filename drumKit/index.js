810691806d28db64e7c16f271beae3dabb064114
OleksandrDikov <o.dikov@luxnet.io>
2024-01-10 21:34:37 +0200

    add js projects

---
+++ drumKit/index.js
@@ -0,0 +1,44 @@
+for (let i=0; i<document.querySelectorAll("button.drum").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        playSound(checkKey(this.innerHTML));
+    });
+}
+
+document.addEventListener("keydown", (event) => {
+    playSound(checkKey(event.key));
+});
+
+function playSound(sound) {
+    let audio = new Audio(sound);
+    audio.play();
+}
+
+function checkKey(key) {
+    let sound
+    switch (key) {
+        case "w":
+            sound = "sounds/tom-1.mp3";
+            break;
+        case "a":
+            sound = "sounds/tom-3.mp3";
+            break;
+        case "s":
+            sound = "sounds/tom-4.mp3";
+            break;
+        case "d":
+            sound = "sounds/tom-2.mp3";
+            break;
+        case "j":
+            sound = "sounds/crash.mp3";
+            break;
+        case "k":
+            sound = "sounds/kick-bass.mp3";
+            break;
+        case "l":
+            sound = "sounds/snare.mp3";
+            break;
+        default:
+            console.log(this);
+    }
+    return sound;
+}
---
+++ testWebStorm/index.js
@@ -0,0 +1,5 @@
+document.querySelector("button").style.color = "red";
+document.querySelector("a").attributes;
+
+//document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href", "https://24tv.ua");
